0ea6c96b622aaad4ee01fe2cc6674fff7472fb95
Altieres de Matos <altieres.matos@db1.com.br>
2018-08-03 18:40:59 -0300

    Update installs

--- variables.sh
+++ variables.sh
@@ -6,6 +6,10 @@ export JAVA_HOME=/opt/java/jdk8
 export MAVEN_HOME=/opt/maven/maven-3.5.4
 export PATH=$PATH:/opt/java/jdk8/bin:/opt/maven/maven-3.5.4/bin
 export SPRING_DATA_MONGODB_URI="mongodb://patiolegal:patiolegal@cluster0-shard-00-00-4v0f7.mongodb.net:27017,cluster0-shard-00-01-4v0f7.mongodb.net:27017,cluster0-shard-00-02-4v0f7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.39.225:2376"
+export DOCKER_CERT_PATH="/home/altieres.matos/.minikube/certs"
+export DOCKER_API_VERSION="1.35"
 EOL
 
 echo Reload Bash
