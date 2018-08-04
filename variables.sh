20c99c35977142b3510cb43b3efd7ab5196054d0
Altieres de Matos <altitdb@gmail.com>
2018-08-04 16:57:03 -0300

    Updating variables

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@ sudo cat >> ~/.bashrc <<EOL
 # Java Variables
 export JDK8=/opt/java/jdk8
 export JAVA_HOME=/opt/java/jdk8
-export PATH=$PATH:/opt/java/jdk8/bin
+export PATH=$PATH:/opt/java/jdk8/bin:/opt/maven/maven-3.5.4/bin
 export SPRING_DATA_MONGODB_URI="mongodb://patiolegal:patiolegal@cluster0-shard-00-00-4v0f7.mongodb.net:27017,cluster0-shard-00-01-4v0f7.mongodb.net:27017,cluster0-shard-00-02-4v0f7.mongodb.net:27017/patiolegal?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://192.168.39.225:2376"
