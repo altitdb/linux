7529519bb048b15defaf9d8cf554fea53be69507
Altieres de Matos <altitdb@gmail.com>
2018-09-27 14:26:57 -0300

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -5,7 +5,6 @@ export JDK8=/opt/java/jdk8
 export JDK10=/opt/java/jdk10
 export JAVA_HOME=/opt/java/jdk10
 export PATH=$PATH:/opt/java/jdk10/bin:/opt/maven/maven-3.5.4/bin
-export SPRING_DATA_MONGODB_URI="mongodb://patiolegal:patiolegal@cluster0-shard-00-00-4v0f7.mongodb.net:27017,cluster0-shard-00-01-4v0f7.mongodb.net:27017,cluster0-shard-00-02-4v0f7.mongodb.net:27017/patiolegal?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
 EOL
 
 echo Reload Bash
