d95dccfa3745dcb72a0c1eb10ebaf913dc279266
Altieres de Matos <altieres.matos@db1.com.br>
2019-01-18 14:00:55 -0200

    Add alias ports

--- variables.sh
+++ variables.sh
@@ -3,8 +3,12 @@ sudo cat >> ~/.bashrc <<EOL
 # Java Variables
 export JDK8=/opt/java/jdk8
 export JDK10=/opt/java/jdk10
-export JAVA_HOME=/opt/java/jdk10
-export PATH=$PATH:/opt/java/jdk10/bin:/opt/maven/maven-3.5.4/bin
+export JDK11=/opt/java/jdk11
+export JAVA_HOME=/opt/java/jdk11
+export PATH=$PATH:/opt/java/jdk11/bin:/opt/maven/maven-3.5.4/bin
+
+# Alias
+alias ports='sudo lsof -i -P -n | grep LISTEN'
 EOL
 
 echo Reload Bash
