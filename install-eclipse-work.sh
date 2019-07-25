e89c7549b093a74cfe4bf25974ecbb05f994d3d5
Altieres de Matos <altieres.matos@db1.com.br>
2019-07-25 18:18:04 -0300

    Update eclipse work

--- install-eclipse-work.sh
+++ install-eclipse-work.sh
@@ -6,9 +6,9 @@ sudo cat > /usr/share/applications/eclipse.desktop <<EOL
 [Desktop Entry]
 Name=Eclipse
 Type=Application
-Exec=/opt/eclipse/java-photon/eclipse/eclipse -vm $JAVA_HOME/bin
+Exec=/opt/eclipse/java-2019-06/eclipse/eclipse -vm $JAVA_HOME/bin
 Terminal=false
-Icon=/opt/eclipse/java-photon/eclipse/icon.xpm
+Icon=/opt/eclipse/java-2019-06/eclipse/icon.xpm
 Comment=Integrated Development Environment
 NoDisplay=false
 Categories=Development;
@@ -16,4 +16,4 @@ Name[en]=Eclipse
 EOL
 
 sudo desktop-file-install /usr/share/applications/eclipse.desktop
-sudo ln -sf /opt/eclipse/java-photon/eclipse /usr/local/bin/
+sudo ln -sf /opt/eclipse/java-2019-06/eclipse /usr/local/bin/
